3bbdc190884f02de309758b76cbf798532c1d5fb
Abdullahi Adedayo <abdullahi.adedayo@gmail.com>
2025-09-21 18:18:49 +0100

    Binary Search and Logger

--- src/main/java/algorithms/Main.java
+++ src/main/java/algorithms/Main.java
@@ -1,13 +1,24 @@
 package algorithms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static algorithms.binarysearch.BinarySearch.binarySearch;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int target = 78;
+        
+        logger.debug("Starting binary search for target: {}", target);
         int result = binarySearch(array, target);
-        System.out.println("Target found at index: " + result);
+        
+        if (result == -1) {
+            logger.info("Target {} not found in the array", target);
+        } else {
+            logger.info("Target {} found at index: {}", target, result);
+        }
     }
 }
